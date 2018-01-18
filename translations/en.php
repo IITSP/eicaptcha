0fb898b22077806baabc4527ae5d5121d14b528f
Alex-Glebov <alex.glebov@iitsp.com.au>
2018-01-19 05:26:11 +0800

    English

---
+++ translations/en.php
@@ -0,0 +1,12 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_3e87eed4786fe702ae961b55bbf02c6a'] = '(Re)Captcha';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_d3c016a5fdfcf8f9396a9ac1fb02a004'] = 'Add a (re) captcha to the prestashop contact form';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_793b58515cefe26f6a3c5ab782460a69'] = 'Captcha mode must be configured';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_feee70b268da9eeeaeba8a837477db0d'] = 'Configuration the captcha';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_77f682c46c4c98d39cfb703d3606f505'] = 'o recover your own keys, please click on the following link';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_d2560860c51f895a9871741f0805c39e'] = 'Private key';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_70afdcba0a7135e2d41fc2d23a6f2fd9'] = 'Public key';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_28fe443e74d1760d87986f153fb88bb7'] = 'The code of the captcha is wrong';
